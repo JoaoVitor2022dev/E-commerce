e717be0da91c317dd3158a19c84b5ceef852ee92
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2023-01-24 00:50:34 -0400

    Update Store.jsx

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import{ useEffect, useState } from 'react'
 
 const Store = () => {
+
+ const [ data , setData ] = useState([]); 
+
+ // requisition 
+ useEffect(() => {
+  
+   const fetchData = async () => {
+    
+    const url = "https://api.mercadolibre.com/sites/MLB/search?q=celular";
+
+    const response = await fetch(url); 
+
+    const json =  await response.json();  
+
+     setData(json.results);  
+   }
+
+   fetchData();
+   
+ },[]); 
+
+console.log(data);
+
   return (
-    <div>Store</div>
+    <div>
+      <h1>Store</h1>
+      <div className="products">
+           {
+           data.map((e) => (
+            <div key={e.id}>
+                <h4>{e.title}</h4>
+                <img src={e.thumbnail} alt="" />
+                <h4> R$: {e.price}</h4>
+            </div>
+           ))
+           }
+      </div>
+    </div>
   )
 }
 
